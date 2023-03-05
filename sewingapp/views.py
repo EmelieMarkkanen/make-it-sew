6b4bae8ce20516e137d37faaa4f0470f36516671
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-03-05 11:37:26 +0000

    Add PatternDetail view and html template

--- sewingapp/views.py
+++ sewingapp/views.py
@@ -9,3 +9,16 @@ class PostPattern(generic.ListView):
     queryset = PostPattern.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     context_object_name = 'posted_patterns'
+
+
+class PatternDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = PostPattern.objects.filter(status=1)
+        postpattern = get_object_or_404(queryset, slug=slug)
+        postcomment = postpattern.comments.filter(approved=True).order_by("-created_on")
+
+        return render(request, "pattern_detail.html", {
+                "postpattern": postpattern,
+                "postcomment": postcomment,
+            },
+        )
