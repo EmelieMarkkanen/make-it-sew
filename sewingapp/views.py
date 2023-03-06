6ec308989e89bca5cba6903047b9148b10b9ac5b
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-03-06 17:40:19 +0000

    Add likes var to PatternDetail view and content to template pattern_detail

--- sewingapp/views.py
+++ sewingapp/views.py
@@ -19,13 +19,17 @@ class PatternDetail(View):
         queryset = PP.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         page = 'pattern_detail'
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
         return render(
             request,
             "pattern_detail.html",
             {
                 "post": post,
-                "page": page
+                "page": page,
+                "liked": liked,
             },
         )
 
