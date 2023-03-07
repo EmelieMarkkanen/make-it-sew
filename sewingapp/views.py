6b31aa992a0c231a5dc2eb9c71f48b28a10edf67
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-03-07 23:31:57 +0000

    Update pattern_detail.html template with icons and layout

--- sewingapp/views.py
+++ sewingapp/views.py
@@ -52,7 +52,6 @@ class PatternDetail(View):
         comment_form = CommentForm(data=request.POST)
 
         if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False) 
             comment.post = post
