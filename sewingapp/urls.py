d9e0a1635ad7bc5ca99f53d486394bf64d74ca9c
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-03-07 11:42:47 +0000

    Install Allauth for user authentication, update settings.py, urls.py and requirements.txt

--- sewingapp/urls.py
+++ sewingapp/urls.py
@@ -1,5 +1,5 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 from .views import PatternDetail, PostPattern, AllPatterns
 
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.PostPattern.as_view(), name='index'),
     path('all_patterns/', views.AllPatterns.as_view(), name='all_patterns'),
     path('liked_patterns/', views.LikedPatterns.as_view(), name='liked_patterns'),
+    path('accounts/', include('allauth.urls')),
     path('<slug:slug>/', PatternDetail.as_view(), name='pattern_detail'),
 ]
