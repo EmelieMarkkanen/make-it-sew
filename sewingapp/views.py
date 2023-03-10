bbfe480027fca081a7f803c28c8da1c361f3dd2c
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-03-10 18:35:57 +0000

    Update PatternDetail view comment function with login required mixin

--- sewingapp/views.py
+++ sewingapp/views.py
@@ -45,6 +45,9 @@ class PatternDetail(View):
         )
 
     def post(self, request, slug, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        
         queryset = PP.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
