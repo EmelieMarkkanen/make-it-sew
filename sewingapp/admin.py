1166de5a0f97d6b70b52365f64c2f85862bc5cc2
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-03-14 14:22:20 +0000

    Add docstrings to admin.py and apps.py

--- sewingapp/admin.py
+++ sewingapp/admin.py
@@ -5,6 +5,10 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(PostPattern)
 class PostAdmin(SummernoteModelAdmin):
+    """
+    Class for the Pattern section of the admin area. 
+    Organises the display of posts, filtering and searchbar.
+    """
 
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
@@ -15,6 +19,10 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(PostComment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Class for the Comment section of the admin area. 
+    Organises the display of comments, filtering and searchbar.
+    """
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
--- sewingapp/apps.py
+++ sewingapp/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class SewingappConfig(AppConfig):
+    """
+    sewingapp config class
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sewingapp'
